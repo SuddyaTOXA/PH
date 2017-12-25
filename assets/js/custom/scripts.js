603f2c1e287ceb8a0179efda34035c64cfff1d9e
Anton <suddyadred@gmail.com>
2017-12-25 17:55:16 +0200

    Suddya

--- assets/js/custom/scripts.js
+++ assets/js/custom/scripts.js
@@ -1,12 +1,4 @@
 jQuery(document).ready(function($) {
-    // header fade
-    // $(function () {
-    //     var header = $('#header');
-    //     setTimeout(function () {
-    //         header.addClass('show');
-    //     }, 800);
-    // });
-
     // header scroll
     var header = $('#header');
     $(window).on('scroll', function () {
@@ -20,15 +12,6 @@ jQuery(document).ready(function($) {
             header.removeClass('scrolling');
         }
     });
-    // for smooth scroll
-    // if ($('a').is('.smooth-scroll')) {
-    //     smoothScroll.init({
-    //         selector: '.smooth-scroll', // Selector for links (must be a class, ID, data attribute, or element tag)
-    //         speed: 500, // Integer. How fast to complete the scroll in milliseconds
-    //         easing: 'easeInQuad', // Easing pattern to use
-    //         offset: 130 // Integer. How far to offset the scrolling anchor location in pixels
-    //     });
-    // }
 
     $(document).ready(function () {
         // for burger menu
